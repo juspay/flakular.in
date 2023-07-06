ff2cdb0786c581a22568f4f7343bd18c20552c7f
Sridhar Ratnakumar <srid@srid.ca>
2023-07-06 16:03:27 -0400

    Set initial metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,12 +13,12 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">🚧🚧 {siteConfig.tagline} 🚧🚧</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Get Inspired - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -30,8 +30,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="How to use Nix flakes and flake-parts for project development.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
