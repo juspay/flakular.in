30e5c1b3eb7522894954af22531f3a2a8ad45f00
Sridhar Ratnakumar <srid@srid.ca>
2023-07-06 17:53:07 -0400

    Add /about page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,6 +71,11 @@ const config = {
             label: 'Content',
           },
           {
+            to: 'about',
+            position: 'left',
+            label: 'About',
+          },
+          {
             href: 'https://github.com/juspay/zero-to-flakes',
             label: 'GitHub',
             position: 'right',
---
+++ src/pages/about.md
@@ -0,0 +1,9 @@
+---
+title: About
+---
+
+# About
+
+:::note
+TODO Write this
+:::
