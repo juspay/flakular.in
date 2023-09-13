e0ce86a92b5f374d026b3ae1fcdd00d299ecb356
Sridhar Ratnakumar <srid@srid.ca>
2023-09-13 10:24:59 -0400

    docusaurus: use logo as social preview (for now)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,7 +141,8 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      // image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo.svg',
       /*
       announcementBar: {
         id: 'wip',
